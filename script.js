e242f08f78f038970eb41b8dd3e8d3ff0a08a69e
AlimShaikh55 <shaikhalim55@gmail.com>
2026-04-05 17:30:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Counter animation
+// Counter Animation
 const counters = document.querySelectorAll('.counter');
 
 counters.forEach(counter => {
   const update = () => {
     const target = +counter.getAttribute('data-target');
-    const current = +counter.innerText;
+    const count = +counter.innerText;
 
     const increment = target / 100;
 
-    if (current < target) {
-      counter.innerText = Math.ceil(current + increment);
-      setTimeout(update, 30);
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(update, 20);
     } else {
       counter.innerText = target;
     }
